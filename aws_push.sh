6f5b7c514699ed24ae59fa11044980786355140c
tharindu_rathgamaguruge <tharindu.rathgamaguruge@tuni.fi>
2024-01-05 07:49:20 +0000

    aws deployment related changes

---
+++ aws_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Set AWS credentials
+export AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY"
+export AWS_DEFAULT_REGION="$AWS_DEFAULT_REGION"
+
+# Set the names of your Docker images
+images=( "service1" "service2" "api_gateway_service" "monitoring_service" )
+
+# Log in to AWS ECR
+aws ecr get-login-password --region $AWS_DEFAULT_REGION | docker login --username AWS --password-stdin $ECR_REPO_URL
+
+# Push Docker images to AWS ECR
+for image in "${images[@]}"; do
+  docker tag $image:latest $ECR_REPO_URL/$image:latest
+  docker push $ECR_REPO_URL/$image:latest
+done
+
+# Clean up - log out from AWS ECR
+docker logout $ECR_REPO_URL
